b22d6aa17607fc5e0545702314b58bf039f4d87f
chegx <110959546+chegx@users.noreply.github.com>
2022-12-03 02:12:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
+document.querySelectorAll(".faq").forEach(function(faq) {
 
-
-
-
-document.querySelectorAll("button").forEach(function(btn) {
-
-  btn.onclick = function() {
-    var index = this.getAttribute("id");
-      this.classList.toggle("dark-bold");
-    document.querySelectorAll(".arrow")[index].classList.toggle("rotate");
-
-    document.querySelectorAll('span')[index].classList.toggle("show");
+  faq.onclick = function() {
+    faq.classList.toggle("active");
+    faq.classList.toggle("rotate");
+    faq.classList.toggle("dark-bold");
   };
 });
